fc625b28d3683c81706597092e0fdcf567dd0c44
Felix Musau <fellyMfelix@gmail.com>
2026-03-06 11:29:38 +0300

    Good feeling

--- backend/server.js
+++ backend/server.js
@@ -33,4 +33,6 @@ app.use('/api', medicalNewsRoutes)
 app.get('/api/health', (req, res) => res.json({ok:true}))
 
 const port = process.env.PORT || 10000;
-app.listen(port, () => console.log(`Backend listening on port ${port}`));
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Backend listening on port ${port}`)
+});
